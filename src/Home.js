a3d14deb05c95247c59ecebaafcd7236e9fd50e6
haafeezdr <haafeezdaiyab>
2023-11-10 15:52:39 +0100

    update

--- src/Home.js
+++ src/Home.js
@@ -1,20 +1,16 @@
 import { useState } from 'react';
+import Bloglist from './Bloglist';
 
 const Home = () => {
         const [blogs, setBlogs] = useState([
-          { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-          { title: 'Welcome party', body: 'lorem ipsum...', author: 'mario', id: 2 },
-          { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+          { title: 'My new website', body: 'lorem ipsum...', author: 'haafeez', id: 1 },
+          { title: 'Welcome party', body: 'lorem ipsum...', author: 'idris', id: 2 },
+          { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'hadi', id: 3 }
         ]);
     
     return ( 
         <div className="home">
-            {blogs.map((blog) => (
-                <div className="blog-preview" key={blog.id}>
-                    <h2>{ blog.title }</h2>
-                     <p>Written by { blog.author }</p>
-                </div>
-            ))}
+            <Bloglist blogs={blogs} title="All Blogs!" />
         </div>
      );
 }
